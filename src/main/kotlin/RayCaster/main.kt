21dd87a2048ad5007527dd24e2549838e909b1ae
Steven Knight <steven.knight@cashstar.com>
2018-09-03 21:44:17 -0400

    Remove unnecessary {} in string templates.

--- src/main/kotlin/RayCaster/main.kt
+++ src/main/kotlin/RayCaster/main.kt
@@ -1,11 +1,11 @@
 package RayCaster
 
 fun main(args: Array<String>) {
-    var nx = 200
-    var ny = 100
-    println("P3\n${nx} ${ny}\n255\n")
+    val nx = 200
+    val ny = 100
+    println("P3\n$nx $ny\n255\n")
 
-    val b = 0.2;
+    val b = 0.2
 
     for (j in ny - 1 downTo 0) {
         for (i in 0 until nx) {
@@ -14,7 +14,7 @@ fun main(args: Array<String>) {
             val ir = (255.99  * r).toInt()
             val ig = (255.99  * g).toInt()
             val ib = (255.99  * b).toInt()
-            println("${ir} ${ig} ${ib}\n")
+            println("$ir $ig $ib\n")
         }
     }
 }
